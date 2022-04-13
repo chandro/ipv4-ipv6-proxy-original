47eb38288bc4cdf78044b03b7b0a52045940b641
Alejandro <chandro@users.noreply.github.com>
2022-04-13 10:35:55 -0500

    Update install.sh
    updated 3proxy version

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,9 +13,9 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.4.tar.g"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
@@ -79,7 +79,7 @@ $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
+yum -y install nano gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
